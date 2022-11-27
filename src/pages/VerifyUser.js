8f229adc87dfea73fc02ebc6cd62f4a0cce8b3f1
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-11-27 12:04:39 -0500

    fix

--- src/pages/VerifyUser.js
+++ src/pages/VerifyUser.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { verifyUserThunk } from '../features/user/userSlice'
 
 const VerifyUser = () => {
   const { id } = useParams()
+  const navigate = useNavigate()
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(verifyUserThunk(id))
+    navigate('/')
     // eslint-disable-next-line
   }, [])
   return <div>VerifyUser</div>
