db2810155d3d435b4d701a139bcafa7139e02090
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-12-09 09:42:46 -0500

    fix

--- src/pages/dashboardPages/Dashboard.js
+++ src/pages/dashboardPages/Dashboard.js
@@ -1,21 +1,26 @@
 import React from 'react'
-import { Helmet } from 'react-helmet-async'
 import styled from 'styled-components'
-
+import emptyCart from '../../images/empty_cart.svg'
 const Dashboard = () => {
   return (
     <Wrapper>
-      <Helmet>
-        <title>Dashboard</title>
-        <meta name='description' content='Welcome to your Dashboard.' />
-        <link rel='canonical' href='/Dashboard' />
-      </Helmet>
-      Dashboard page
+      <div className='empty-cart'>
+        <h3>Your cart is empty.</h3>
+        <img src={emptyCart} alt='' />
+      </div>
+      <div className='cart'></div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
   min-height: calc(100vh - 3.2rem);
+  .empty-cart {
+    display: grid;
+    place-content: center;
+    img {
+      width: 400px;
+    }
+  }
 `
 export default Dashboard
