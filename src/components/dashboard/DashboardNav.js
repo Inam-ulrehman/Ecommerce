f111b3436f918b64219e86ab8611b3db34634b13
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-12-12 12:00:00 -0500

    fix

--- src/components/dashboard/DashboardNav.js
+++ src/components/dashboard/DashboardNav.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { dashboardNavLink } from '../../utils/data'
 
 const DashboardNav = () => {
+  const [value, setValue] = useState(0)
   const { user } = useSelector((state) => state)
   return (
     <Wrapper>
       <ul>
         {dashboardNavLink.map((item, index) => {
           return (
-            <li key={index}>
-              <Link to={item.path}>{item.title}</Link>
+            <li onClick={() => setValue(index)} key={index}>
+              <Link
+                className={index === value ? 'btn active' : 'btn'}
+                to={item.path}
+              >
+                {item.title}
+              </Link>
             </li>
           )
         })}
@@ -31,22 +37,31 @@ const Wrapper = styled.div`
   justify-content: space-between;
   ul {
     display: flex;
+    flex-wrap: wrap;
     li {
-      padding: 0 1rem;
-      a {
-        color: black;
-      }
+      padding: 0.2rem 0.3rem;
     }
   }
   p {
-    color: black;
+    color: var(--white);
     padding: 0 1rem;
+    margin: 0;
     strong {
       border-bottom: 2px solid var(--primary-8);
     }
   }
 
-  background-color: var(--primary-1);
+  background-color: var(--primary-8);
+
+  .btn {
+    background: var(--primary-8);
+    :hover {
+      background: var(--primary-5);
+    }
+  }
+  .active {
+    background: var(--primary-5);
+  }
 `
 
 export default DashboardNav
