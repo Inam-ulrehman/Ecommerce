2e3ebef666d3be218d31eb26e47ab26252b80f5c
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-01-23 13:35:40 -0500

    fix

--- src/pages/booking/Booking.js
+++ src/pages/booking/Booking.js
@@ -1,31 +1,183 @@
+import moment from 'moment/moment'
 import React from 'react'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+import FormInput from '../../components/FormInput'
+import { slot } from '../../utils/data'
 
 const initialState = {
+  category: '',
   date: '',
+  bookingId: '',
 }
+
 const Booking = () => {
   const [state, setState] = useState(initialState)
+  const { initialProductList } = useSelector((state) => state.product)
+
+  const uniqueCategory = [
+    'select',
+    ...new Set(initialProductList.map((item) => item.category)),
+  ]
+  moment(state.date).format('dddd')
+
+  // handle Submit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submit form')
+  }
 
   const handleChange = (e) => {
     const name = e.target.name
     const value = e.target.value
-    console.log(name)
+    setState({ ...state, [name]: value })
+  }
+
+  const handleBooking = async (_id) => {
+    setState({ ...state, bookingId: _id })
   }
   return (
-    <div>
+    <Wrapper>
+      {/* ===== Category ====== */}
+
       <div>
-        <h1>Please pick a date</h1>
+        <label htmlFor='category'>Pick a category</label>
+        <select
+          name='category'
+          id='category'
+          value={state.category}
+          onChange={handleChange}
+        >
+          {uniqueCategory.map((item, index) => {
+            return (
+              <option key={index} value={item}>
+                {item}
+              </option>
+            )
+          })}
+        </select>
       </div>
-      <input
-        type='date'
-        name='date'
-        id='date'
-        value={state.date}
-        onChange={handleChange}
-      />
-    </div>
+
+      {/* ====date======= */}
+
+      {state.category && state.category !== 'select' && (
+        <div>
+          <div>
+            <h1>Please pick a date</h1>
+          </div>
+          <input
+            type='date'
+            name='date'
+            id='date'
+            value={state.date}
+            onChange={handleChange}
+          />
+        </div>
+      )}
+
+      {/* ======Show day====== */}
+      {moment(state.date).format('dddd') === 'Sunday' && (
+        <h4>Sorry we're closed sundays.</h4>
+      )}
+      {state.category &&
+        state.category !== 'select' &&
+        state.date &&
+        moment(state.date).format('dddd') !== 'Sunday' && (
+          <div className='day-container'>
+            <div className='date-holder'>
+              <span>{moment(state.date).format('dddd')} </span>
+              <span>Available Dates</span>
+            </div>
+            <div className='day-body'>
+              {slot.map((item, index) => {
+                return (
+                  <div
+                    className={
+                      item._id === state.bookingId
+                        ? 'day-holder active'
+                        : 'day-holder'
+                    }
+                    onClick={() => handleBooking(item._id)}
+                    key={index}
+                  >
+                    {item.startTime} - {item.endTime}
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
+      {/* =====Customer Details ====== */}
+      {state.category &&
+        state.category !== 'select' &&
+        state.date &&
+        state.bookingId &&
+        moment(state.date).format('dddd') !== 'Sunday' && (
+          <div className='customer-details'>
+            <form className='form' onSubmit={handleSubmit}>
+              {/* name */}
+              <FormInput
+                name='name'
+                value={state.name}
+                onChange={handleChange}
+              />
+              {/* email */}
+              <FormInput
+                name='email'
+                value={state.email}
+                onChange={handleChange}
+              />
+              {/* Note */}
+              <div>
+                <label className='form-label' htmlFor='note'>
+                  Note
+                </label>
+                <textarea
+                  className='form-textarea'
+                  name='note'
+                  id='note'
+                  cols='30'
+                  rows='10'
+                ></textarea>
+              </div>
+              <button type='submit' className='btn'>
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div`
+  .day-container {
+    .date-holder {
+      span {
+        margin: 1rem;
+      }
+    }
+
+    .day-body {
+      display: flex;
+      flex-wrap: wrap;
+      border: 2px solid black;
+    }
+    .day-holder {
+      box-shadow: var(--shadow-1);
+      margin: 1rem;
+      padding: 1rem;
+      transition: var(--transition-1);
+      :hover {
+        background-color: var(--green-light);
+
+        cursor: pointer;
+      }
+    }
+  }
+  .active {
+    background-color: var(--green-light);
+  }
+`
 export default Booking
--- src/utils/data.js
+++ src/utils/data.js
@@ -28,7 +28,27 @@ export const dashboardNavLink = [
   { id: 3, title: 'Profile', path: '/dashboard/profile' },
   { id: 4, title: 'Change password', path: '/dashboard/changepassword' },
 ]
-// ==================footer //============
+// ======================time ============
+export const slot = [
+  { _id: 1, startTime: '07:00', endTime: '07:30' },
+  { _id: 2, startTime: '07:30', endTime: '08:00' },
+  { _id: 3, startTime: '08:00', endTime: '08:30' },
+  { _id: 4, startTime: '08:30', endTime: '09:00' },
+  { _id: 5, startTime: '09:00', endTime: '09:30' },
+  { _id: 6, startTime: '09:30', endTime: '10:00' },
+  { _id: 7, startTime: '10:00', endTime: '10:30' },
+  { _id: 8, startTime: '10:30', endTime: '11:00' },
+  { _id: 9, startTime: '11:00', endTime: '12:30' },
+  { _id: 10, startTime: '12:30', endTime: '13:00' },
+  { _id: 11, startTime: '13:00', endTime: '13:30' },
+  { _id: 12, startTime: '13:30', endTime: '14:00' },
+  { _id: 13, startTime: '14:00', endTime: '14:30' },
+  { _id: 14, startTime: '14:30', endTime: '15:00' },
+  { _id: 15, startTime: '15:00', endTime: '15:30' },
+  { _id: 16, startTime: '15:30', endTime: '16:00' },
+  { _id: 17, startTime: '16:00', endTime: '16:30' },
+  { _id: 18, startTime: '16:30', endTime: '17:00' },
+]
 
 // ======== Stripe  ==========
 
